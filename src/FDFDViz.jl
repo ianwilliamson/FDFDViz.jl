1e6e2c0e1f971acb0243d41fc9d46148c34d2987
Ian Williamson <iwill@stanford.edu>
2018-07-05 16:20:13 -0700

    Text tweaks

--- src/FDFDViz.jl
+++ src/FDFDViz.jl
@@ -5,14 +5,14 @@ using FDFD, PyPlot, PyCall, AxisArrays
 export plot_field, plot_device, add_scalebar, add_wavelengthbar
 
 "    plot_field(field::Field; cbar::Bool=false, funcz=real)"
-function plot_field(field::Field; cbar::Bool=false, funcz=real)
+function plot_field(field::Field; cbar::Bool=false, funcz=real, showtext=true)
 	fig, ax = subplots(1);
-	plot_field(ax, field; cbar=cbar, funcz=funcz);
+	plot_field(ax, field; cbar=cbar, funcz=funcz, dotext=dotext);
 	return ax
 end
 
 "    plot_field(ax::PyObject, field::Field; cbar::Bool=false, funcz=real)"
-function plot_field(ax::PyObject, field::Field; cbar::Bool=false, funcz=real)
+function plot_field(ax::PyObject, field::Field; cbar::Bool=false, funcz=real, showtext=true)
 	isa(field, FieldTM) && (Z = funcz.(field[:,:,:Ez]).');
 	isa(field, FieldTE) && (Z = funcz.(field[:,:,:Hz]).');
 
@@ -32,11 +32,13 @@ function plot_field(ax::PyObject, field::Field; cbar::Bool=false, funcz=real)
 	extents = [ field.grid.bounds[1][1], field.grid.bounds[2][1],
 	            field.grid.bounds[1][2], field.grid.bounds[2][2] ];
 
-	mappable = ax[:imshow](Z, cmap=cmap, extent=extents, origin="lower", vmin=vmin, vmax=vmax);
+	mappable = ax[:imshow](Z, cmap=cmap, extent=extents, origin="lower", vmin=vmin, vmax=vmax, rasterized=true);
 	cbar && colorbar(mappable, ax=ax, label=L"$\vert E \vert$");
-	ax[:set_xlabel](L"$x$");
-	ax[:set_ylabel](L"$y$");
-	ax[:set_title](@sprintf("ω/2π = %.2f THz", real(field.ω/2π/1e12)));
+	showtext && ax[:set_xlabel](L"$x$");
+	showtext && ax[:set_ylabel](L"$y$");
+	showtext && ax[:set_title](@sprintf("ω/2π = %.2f THz", real(field.ω/2π/1e12)));
+	~showtext && ax[:set_xticks]([])
+	~showtext && ax[:set_yticks]([])
 end
 
 "    plot_device(device::AbstractDevice; outline::Bool=false)"
@@ -46,8 +48,8 @@ function plot_device(device::AbstractDevice; outline::Bool=false)
 	return ax
 end
 
-"    plot_device(ax::PyObject, device::AbstractDevice; outline::Bool=false, lc::String=\"k\", lcm::String=\"k\")"
-function plot_device(ax::PyObject, device::AbstractDevice; outline::Bool=false, lc::String="k", lcm::String="k")
+"    plot_device(ax::PyObject, device::AbstractDevice; outline::Bool=false, lc::String=\"k\", lcm1::String=\"r\", lcm1::String=\"g\")"
+function plot_device(ax::PyObject, device::AbstractDevice; outline::Bool=false, lc::String="k", lcm1::String="r", lcm2::String="g")
 	Z = real.(device.ϵᵣ)';
 	Zi = imag.(device.ϵᵣ)';
 
@@ -57,17 +59,17 @@ function plot_device(ax::PyObject, device::AbstractDevice; outline::Bool=false, 
 	else
 		extents = [ device.grid.bounds[1][1], device.grid.bounds[2][1],
 		            device.grid.bounds[1][2], device.grid.bounds[2][2] ];
-		ax[:imshow](Z, extent=extents, origin="lower", cmap="YlGnBu");
+		ax[:imshow](Z, extent=extents, origin="lower", cmap="YlGnBu", rasterized=true);
 	end
 
 	ax[:contour](xc(device.grid), yc(device.grid), Zi, colors=lc, linewidths=0.5, linestyles=":");
 
 	if isa(device, ModulatedDevice)
 		Z2 = abs.(device.Δϵᵣ)';
-		ax[:contour](xc(device.grid), yc(device.grid), Z2, levels=1, linewidths=0.5, colors=lcm);
+		ax[:contour](xc(device.grid), yc(device.grid), Z2, levels=1, linewidths=0.5, colors=lcm1);
+		Z2 = real.(device.Δϵᵣ)';
+		ax[:contour](xc(device.grid), yc(device.grid), Z2, levels=1, linewidths=0.5, colors=lcm2);
 	end
-	ax[:set_xlabel](L"$x$");
-	ax[:set_ylabel](L"$y$");
 end
 
 "    add_scalebar(ax::PyObject, pt::Point; fc::String=\"k\", width::Number=1.0, height::Number=0.125, hide::Bool=true)"
